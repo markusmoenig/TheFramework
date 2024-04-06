783622da29354a17fc3eeddc7500f8dca778f44a
Markus Moenig <markus@moenig.io>
2024-04-06 12:52:50 +0700

    Load Paint.NET palettes

--- src/thepalette.rs
+++ src/thepalette.rs
@@ -38,6 +38,72 @@ impl ThePalette {
     pub fn get_current_color(&self) -> Option<TheColor> {
         self.colors[self.current_index as usize].clone()
     }
+
+    /// Load the palette from a Paint.net TXT file
+    pub fn load_from_txt(&mut self, txt: String) {
+        let mut index = self.current_index as usize;
+        for line in txt.lines() {
+            // Ignore comments
+            if line.starts_with(';') {
+                continue;
+            }
+
+            let mut chars = line.chars();
+
+            // Skip Alpha
+            if chars.next().is_none() {
+                return;
+            }
+            if chars.next().is_none() {
+                return;
+            }
+
+            // R
+            let mut r_string = "".to_string();
+            if let Some(c) = chars.next() {
+                r_string.push(c);
+            }
+            if let Some(c) = chars.next() {
+                r_string.push(c);
+            }
+
+            let r = u8::from_str_radix(&r_string, 16);
+
+            // G
+            let mut g_string = "".to_string();
+            if let Some(c) = chars.next() {
+                g_string.push(c);
+            }
+            if let Some(c) = chars.next() {
+                g_string.push(c);
+            }
+
+            let g = u8::from_str_radix(&g_string, 16);
+
+            // B
+            let mut b_string = "".to_string();
+            if let Some(c) = chars.next() {
+                b_string.push(c);
+            }
+            if let Some(c) = chars.next() {
+                b_string.push(c);
+            }
+
+            let b = u8::from_str_radix(&b_string, 16);
+
+            if r.is_ok() && g.is_ok() && b.is_ok() {
+                let r = r.ok().unwrap();
+                let g = g.ok().unwrap();
+                let b = b.ok().unwrap();
+
+                if index < self.colors.len() {
+                    self.colors[index] = Some(TheColor::from_u8(r, g, b, 0xFF));
+                }
+
+                index += 1;
+            }
+        }
+    }
 }
 
 impl Index<usize> for ThePalette {
